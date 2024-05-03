2219a02612d522365a3e4a4afb0dd4f862e03030
Carmelo Gulino <s298109@studenti.polito.it>
2024-05-03 23:00:05 +0200

    prima ottimizzazione

--- model/model.py
+++ model/model.py
@@ -1,5 +1,5 @@
 import copy
-from functools import cache, lru_cache
+from time import time
 from database.DAO import DAO
 
 
@@ -11,13 +11,18 @@ class Model:
         self.loadNerc()
         self.soluzioni = []
         self.iterazioni = 0
-        self.costo_cumulato = 0
+        self.costo_cumulato = 0  #caching per tenere conto del cumulato delle ore
+        self.recente = 2015
+        self.vecchio = 1999  #caching per tenere conto dell'anno più recente e più vecchio
 
     def worstCase(self, nerc, maxY, maxH):
         self._listEvents = []  #ogni volta che chiamo la funzione resetto la lista di eventi
         self.loadEvents(nerc)  #carico gli eventi che mi interessano a seconda del nerc scelto
-        self.costo_cumulato = 0
+        self.costo_cumulato = 0  #a ogni chiamata azzero il cumulato
+        t1 = time()
         self.ricorsione(set(), maxY, maxH, 0)  #parziale è una lista
+        t2 = time()
+        print(t2 - t1)
 
     def ricorsione(self, parziale, maxY, maxH, pos):  #possibili è l'insieme totale di eventi
         # CASO BANALE
@@ -31,52 +36,41 @@ class Model:
             for evento in self._listEvents[pos:]:
                 pos += 1
                 parziale.add(evento)  # aggiungo l'evento al parziale
-                if self.condizioni(parziale, maxY, maxH):  #controllo le condizioni
+                if self.condizioni(evento, maxY, maxH):  #controllo le condizioni
                     self.ricorsione(parziale, maxY, maxH, pos)  #faccio ripartire la ricorsione con la copia
-                parziale.remove(evento)
+                self.backtracking(parziale, evento)
 
-    def condizioni(self, parziale, maxY, maxH):
-        if self.condizione_durata(parziale, maxH) and self.condizione_anni(parziale, maxY):
+    def backtracking(self, parziale, evento):
+        parziale.remove(evento)
+        self.costo_cumulato -= (evento.date_event_finished - evento.date_event_began).total_seconds()
+
+    def condizioni(self, evento, maxY, maxH):
+        if self.condizione_durata(evento, maxH) and self.condizione_anni(evento, maxY):
             return True
         else:
             return False
 
-    def condizione_durata(self, parziale, maxH):
+    def condizione_durata(self, evento, maxH):
         '''
-        Verifica che il numero di ore degli eventi considerati non superi la soglia
+        Aggiunge le ore al costo cumulato e verifica che non superino la soglia
         '''
-        s = 0
-        for e in parziale:
-            durata = (e.date_event_finished - e.date_event_began).total_seconds()
-            s += durata
-        if s > maxH * 3600:
+        durata = (evento.date_event_finished - evento.date_event_began).total_seconds()
+        self.costo_cumulato += durata
+        if self.costo_cumulato > maxH * 3600:
             return False
         else:
             return True
 
-    def condizione_anni(self, parziale, maxY):
-        recente = self.trova_piu_recente(parziale)
-        vecchio = self.trova_piu_vecchio(parziale)
-        if recente - vecchio > maxY:
+    def condizione_anni(self, evento, maxY):
+        anno = evento.date_event_began.year
+        if anno < self.recente:
+            self.recente = anno
+        if anno > self.vecchio:
+            self.vecchio = anno
+        if self.recente - self.vecchio > maxY:
             return False
         else:
             return True
-
-    def trova_piu_recente(self, parziale):
-        recente = 2015
-        for e in parziale:
-            anno = e.date_event_began.year
-            if anno < recente:
-                recente = anno
-        return recente
-
-    def trova_piu_vecchio(self, parziale):
-        vecchio = 1999
-        for e in parziale:
-            anno = e.date_event_began.year
-            if anno > vecchio:
-                vecchio = anno
-        return vecchio
 
     def calcola_parametri(self, parziale):
         ppl = self.calcola_persone(parziale)
